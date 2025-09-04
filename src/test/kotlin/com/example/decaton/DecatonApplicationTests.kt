5a54b0c4d0425b0e923ec96f429eca272516e830
Camilodor <camilosecun@gmail.com>
2025-09-04 08:01:01 -0500

    Primer commit: proyecto inicial

---
+++ src/test/kotlin/com/example/decaton/DecatonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.decaton
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DecatonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
